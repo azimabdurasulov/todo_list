cd643e9951b5fbe4b9816db078c737dd7042ae29
azimabdurasulov <azimjon20050604@gmail.com>
2023-03-28 11:19:46 +0500

    init

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class Task(models.Model):
+    task = models.CharField(max_length=50)
+    description = models.TextField()
+    complited = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -1,41 +1,56 @@
-from django.http import HttpResponse, JsonResponse, HttpRequest
+from django.http import HttpResponse, JsonResponse,HttpRequest
 from django.views import View
-import json
 # Import user model
 from django.contrib.auth.models import User
-
+from .models import Task
 # Import authentication classes
 from django.contrib.auth import authenticate
+from base64 import b64decode
 
+def isAuth(auth):
+    if auth is None:
+        return False
+    # Get token
+    token = auth.split(' ')[1]
+    auth=b64decode(token).decode() # Decode token
+    username, password = auth.split(':') # Split token
 
-class HomeView(View):
-    def get(self, request):
-        user = User.objects.all()
-        data = {
-            'users': []
-        }
-        for i in user:
-            data['users'].append({
-                'id': i.id,
-                'username': i.username,
-                'email': i.email
-            })
-        return JsonResponse(data)
-    def post(self, request):
-        return HttpResponse('ok')
-        
-
-
-def index(request: HttpRequest):
-    if request.method == 'POST':
-        decode = request.body.decode()
-        data = json.loads(decode)
-        # Check if user is authenticated
-        user = authenticate(username=data['username'], password=data['passvord'])
-        if user is not None:
-            return JsonResponse({
-                'username': user.username,
-                'passvord': user.password
-            })
-        return JsonResponse({'result': 'You are not authenticated'})
+    # Check if user is authenticated
     
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        return True
+    return False
+
+
+
+def tasks(request: HttpRequest) -> JsonResponse:
+    """
+    Create a task
+    """
+    auth = request.headers.get('Authorization')
+    if isAuth(auth):
+        if request.method == 'POST':
+            return JsonResponse({'message': 'Task created successfully'}, status=201)
+        if request.method == 'GET':
+            todos = {
+                'todo':[]
+            }
+            # Get all tasks
+            tasks = Task.objects.all()
+            for task in tasks:
+                todos['todo'].append({
+                    'id': task.id,
+                    'task': task.task,
+                    'description': task.description,
+                    'complited': task.complited,
+                    'created_at': task.created_at,
+                    'updated_at': task.updated_at,
+                   
+                })
+
+            return JsonResponse(todos, status=200)
+        
+    else:
+        return JsonResponse({'message': 'Unauthorized'}, status=401)
