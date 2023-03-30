dd67eb92bfc6f1fb97cb5493964df61560c363f0
azimabdurasulov <azimjon20050604@gmail.com>
2023-03-30 11:05:58 +0500

    done

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse,HttpRequest
+from django.shortcuts import render
 from django.views import View
 import json
 # Import user model
@@ -26,7 +27,7 @@ def isAuth(auth):
 
 
 
-def tasks(request: HttpRequest) -> JsonResponse:
+def get_tasks(request: HttpRequest) -> JsonResponse:
     """
     Create a task
     """
@@ -75,3 +76,88 @@ def tasks(request: HttpRequest) -> JsonResponse:
         
     else:
         return JsonResponse({'message': 'Unauthorized'}, status=401)
+    
+def get_task_id(request:HttpRequest, pk:int)->JsonResponse:
+    auth = request.headers.get('Authorization')
+    user = isAuth(auth)
+    if user:
+        if request.method == 'GET':
+            try:
+                # get task from database by id
+                task = Task.objects.get(id=pk)
+                return JsonResponse(task.to_dict())
+            except:
+                return JsonResponse({"status": "object doesn't exist"})
+            
+        if request.method == 'POST':
+            decoded = request.body.decode()
+            tasks = json.loads(decoded)
+            task = tasks.get('task',False)
+            description = tasks.get('description',False)
+            completed = tasks.get('completed',False)
+            created_at = tasks.get('created_at',False)
+            updated_at = tasks.get('updated_at',False)
+
+            todos = Task.objects.get(id=pk)
+            if task:
+                todos.task=task
+            if description:
+                todos.description=description
+            if completed:
+                todos.completed=completed
+            if created_at:
+                todos.created_at=created_at
+            if updated_at:
+                todos.updated_at=updated_at
+
+            todos.save()
+            return JsonResponse(todos.to_dict())
+
+def delete_task_id(request: HttpRequest, pk: int) -> JsonResponse:
+    auth = request.headers.get('Authorization')
+    user = isAuth(auth)
+    if user:
+        if request.method == 'POST':
+            try:
+                tasks = Task.objects.get(id=pk)
+                tasks.delete()
+                return JsonResponse(tasks.to_dict())
+            except:
+                return JsonResponse({"status": "object doesn't exist"})
+            
+def completed_task(request: HttpRequest):
+    auth = request.headers.get('Authorization')
+    user = isAuth(auth)
+    if user:
+        try:
+            tasks = Task.objects.all()
+            result = []
+            for task in tasks:
+                if task.completed:
+                    result.append(task.to_dict())
+
+            return JsonResponse({'result':result})
+        except:
+            return HttpResponse('Hello World!')
+
+def incompleted_task(request: HttpRequest):
+    auth = request.headers.get('Authorization')
+    user = isAuth(auth)
+    if user:
+        try:
+            tasks = Task.objects.all()
+            result = []
+            for task in tasks:
+                if task.completed == False:
+                    result.append(task.to_dict())
+
+            return JsonResponse({'result':result})
+        except:
+                return HttpResponse('Hello World!')
+        
+def home(request: HttpRequest):
+    tasks = Task.objects.all()
+    data = {
+        "tasks":tasks
+    }
+    return render(request, 'home.html', context=data)
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import tasks
+from api.views import get_tasks, get_task_id, completed_task, incompleted_task, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', tasks)
+    path('tasks/', get_tasks),
+    path('tasks/<int:pk>', get_task_id),
+    path('tasks/completed', completed_task),
+    path('tasks/incompleted', incompleted_task),
+    path('home/', home)
 ]
