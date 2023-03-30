43a822aa2e534b777050635dff3d80b9bf88a3c5
azimabdurasulov <azimjon20050604@gmail.com>
2023-03-30 09:22:05 +0500

    init

--- api/models.py
+++ api/models.py
@@ -12,3 +12,15 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def to_dict(self):
+        returned = {
+            'id': self.id,
+            'task': self.task,
+            'description': self.description,
+            'complited': self.complited,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'student_id': self.student_id
+        }
+        return returned
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse,HttpRequest
 from django.views import View
+import json
 # Import user model
 from django.contrib.auth.models import User
 from .models import Task
@@ -30,25 +31,45 @@ def tasks(request: HttpRequest) -> JsonResponse:
     Create a task
     """
     auth = request.headers.get('Authorization')
-    if isAuth(auth):
+    user = isAuth(auth)
+    if user:
         if request.method == 'POST':
-            return JsonResponse({'message': 'Task created successfully'}, status=201)
+            decoded = request.body.decode()
+            todos = json.loads(decoded)
+            task = todos.get('task',False)
+            description = todos.get('description', False)
+            completed = todos.get('completed', False)
+            created_at = todos.get('created_at',False)
+            updated_at = todos.get('updated_at', False)
+            student_id = todos.get('student_id', False)
+
+            if task == False:
+                return JsonResponse({'status': 'task field is required'})
+            if description == False:
+                return JsonResponse({"status": 'description field is required'})
+            if student_id == False:
+                return JsonResponse({"status": 'student_id field is required'})
+            
+            tasks = Task(
+                task=task,
+                description=description,
+                completed=completed,
+                created_at=created_at,
+                updated_at=updated_at,
+                student_id=student_id
+            )
+
+            tasks.save()
+            return JsonResponse(tasks.to_dict())
+            # return JsonResponse({'message': 'Task created successfully'}, status=201)
         if request.method == 'GET':
             todos = {
                 'todo':[]
             }
             # Get all tasks
-            tasks = Task.objects.all()
+            tasks = Task.objects.filter(student = user)
             for task in tasks:
-                todos['todo'].append({
-                    'id': task.id,
-                    'task': task.task,
-                    'description': task.description,
-                    'complited': task.complited,
-                    'created_at': task.created_at,
-                    'updated_at': task.updated_at,
-                   
-                })
+                todos['todo'].append(task.to_dict())
 
             return JsonResponse(todos, status=200)
         
